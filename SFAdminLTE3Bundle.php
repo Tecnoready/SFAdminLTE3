006c952e6bd4a933ac6d0f68372ed31cbb53dfa8
Carlos Mendoza <inhack20@gmail.com>
2020-05-24 14:07:12 -0400

    inicial

---
+++ SFAdminLTE3Bundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the SFAdminLTE3Bundle package.
+ *
+ * (c) Tecnoready <https://github.com/Tecnoready/SFAdminLTE3Bundle/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tecnoready\SFAdminLTE3Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Carlos Mendoza <inhack20@gmail.com>
+ */
+class SFAdminLTE3Bundle extends Bundle
+{
+}
