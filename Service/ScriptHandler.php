d260c9ac183ac4b617fbf4c9796926c42d894ac0
Carlos Mendoza <inhack20@gmail.com>
2020-06-04 16:23:05 -0400

    guardando templates fos

---
+++ Service/ScriptHandler.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the SFAdminLTE3Bundle package.
+ *
+ * (c) Tecnoready <https://github.com/Tecnoready/SFAdminLTE3Bundle/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tecnoready\SFAdminLTE3Bundle\Service;
+
+use Composer\Script\Event;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * Manejador de eventos de composer
+ *
+ * @author Carlos Mendoza <inhack20@gmail.com>
+ */
+class ScriptHandler
+{
+    public static function postInstall(Event $event)
+    {
+        self::handle($event);
+    }
+
+    public static function postUpdate(Event $event)
+    {
+        self::handle($event);
+    }
+    
+    protected static function handle(Event $event)
+    {
+        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+        self::installFOSUserBundle($vendorDir);
+    }
+    
+    protected static function installFOSUserBundle($vendorDir)
+    {
+        $fs = new Filesystem();
+        $folderDir = __DIR__."/../Resources/views/FOSUserBundle/layout.html.twig";
+
+        echo "*** tecnoready/sf-adminlte3-bundle: Instalando enlace simbolico a plantilla FOSUserBundle... \t";
+        $targetDir = $vendorDir."/../templates/bundles/FOSUserBundle";
+        $fs->symlink($folderDir, $targetDir);
+
+        echo 'SUCCESS: ';
+        echo sprintf("Se hizo un enlace simbolico de %s a %s \n");
+    }
+}
