5741b0e18bda26fa9844a87f3e7f27d70acb6dd1
Carlos Mendoza <inhack20@gmail.com>
2020-06-04 23:26:53 -0400

    agregando test

---
+++ tests/Service/ScriptHandlerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the SFAdminLTE3Bundle package.
+ *
+ * (c) Tecnoready <https://github.com/Tecnoready/SFAdminLTE3Bundle/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace sdsdsd;
+
+/**
+ * Description of ScriptHandlerTest
+ *
+ * @author Carlos Mendoza <inhack20@gmail.com>
+ */
+class ScriptHandlerTest
+{
+    //put your code here
+}
