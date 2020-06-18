91f2fa049d074558b6c1d4efc97981a2b1e23861
Carlos Mendoza <inhack20@gmail.com>
2020-06-17 23:33:24 -0400

    feat(navbar): se agrega parametro de plantilla del navbar

--- src/Service/TemplateLTEManager.php
+++ src/Service/TemplateLTEManager.php
@@ -41,6 +41,7 @@ class TemplateLTEManager
             "default_avatar" => "bundles/sfadminlte3/img/avatar_m.png",
             "logo" => "build/images/logo.png",
             "route_runtime_js" => null,
+            "right_navbar" => "@SFAdminLTE3/default/right_navbar.html.twig",
         ]);
         $resolver->setDefined(["menu_builder","app_name","user_profile_photo_property","default_avatar","logo","route_main","route_runtime_js"]);
         $resolver->setRequired(["route_main","footer_version","footer_name","footer_href","footer_year"]);
